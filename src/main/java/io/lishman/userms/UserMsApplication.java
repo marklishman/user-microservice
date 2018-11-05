008c5e22c9647ef9fe5b12d0304432b0758f5901
marklishman <mark.lishman@gmail.com>
2018-11-05 12:43:55 +0000

    Reading base path from config server

--- src/main/java/io/lishman/userms/UserMsApplication.java
+++ src/main/java/io/lishman/userms/UserMsApplication.java
@@ -1,26 +1,14 @@
 package io.lishman.userms;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Configuration
 @RestController
 public class UserMsApplication {
-
-    // TODO docker run -p5432:5432 --name ref-db -e POSTGRES_PASSWORD=secret -d postgres
-
-    @Value("${message}")
-    private String name = "Hi there!";
-
-    @GetMapping("/")
-    public String message() {
-        return name;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(UserMsApplication.class, args);
--- src/main/java/io/lishman/userms/config/RestApiConfiguration.java
+++ src/main/java/io/lishman/userms/config/RestApiConfiguration.java
@@ -1,5 +1,6 @@
 package io.lishman.userms.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -8,9 +9,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class RestApiConfiguration implements RepositoryRestConfigurer {
 
+    @Value("${user.basePath}")
+    private String basePath;
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.setBasePath("/api");
+        config.setBasePath(basePath);
         config.setRepositoryDetectionStrategy(RepositoryDetectionStrategy.RepositoryDetectionStrategies.ANNOTATED);
     }
 
