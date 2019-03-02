b09bff17115ddbd5e4476a20e96ba3103bc39193
marklishman <mark.lishman@gmail.com>
2019-03-02 14:12:34 +0000

    Streamlined Removed config server client

--- src/main/java/io/lishman/userms/config/RestApiConfiguration.java
+++ src/main/java/io/lishman/userms/config/RestApiConfiguration.java
@@ -1,6 +1,5 @@
 package io.lishman.userms.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -9,12 +8,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class RestApiConfiguration implements RepositoryRestConfigurer {
 
-    @Value("${user.basePath}")
-    private String basePath;
-
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.setBasePath(basePath);
         config.setRepositoryDetectionStrategy(RepositoryDetectionStrategy.RepositoryDetectionStrategies.ANNOTATED);
     }
 
